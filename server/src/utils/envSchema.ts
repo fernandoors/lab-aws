9d4b07998612f8e414507b8301ce6d6f70a52355
fernandoors92 <fernando@movemate.io>
2023-10-04 16:25:36 -0400

    fix update env

--- server/src/utils/envSchema.ts
+++ server/src/utils/envSchema.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 config()
 const envSchema = z.object({
   URL: z.string().nullable(),
+  lab: z.string().nullable(),
 });
 
 export const env = envSchema.parse(process.env);
