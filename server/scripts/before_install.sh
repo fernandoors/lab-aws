af72a82207c269f0cd29e64d3230bdd7081f6e73
fernandoors92 <fernando@movemate.io>
2023-10-04 15:14:16 -0400

    fix script to not run on echo action

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo "$(date) run before_install.sh: " >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'run before_install.sh: ' >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo "$(date) cd /home/ubuntu/lab-aws/server" >>/home/ubuntu/lab-aws/server/deploy.log
-pwd
-ls
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'cd /home/ubuntu/lab-aws/server' >>/home/ubuntu/lab-aws/server/deploy.log
 cd /home/ubuntu/lab-aws/server >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo "$(date) npm install" >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'npm install' >>/home/ubuntu/lab-aws/server/deploy.log
 npm install >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'npm build' >>/home/ubuntu/lab-aws/server/deploy.log
 npm run build >>/home/ubuntu/lab-aws/server/deploy.log
