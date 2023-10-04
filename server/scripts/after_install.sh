8aee6683cc70ae1dc34568365561dc4e72844de7
fernandoors92 <fernando@movemate.io>
2023-10-04 14:46:25 -0400

    fix set correct folders

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo pwd
-echo "$(date) run after_install.sh: " >>/home/ubuntu/lab-aws/app/server\/deploy.log
-echo "$(date) pm2 status app-server" >>/home/ubuntu/lab-aws/app/server\/deploy.log
-pm2 status app-server >>/home/ubuntu/lab-aws/app/server\/deploy.log
+pwd
+ls
+echo "$(date) run after_install.sh: " >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date) pm2 status app-server" >>/home/ubuntu/lab-aws/server/deploy.log
+pm2 status app-server >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo "$(date) pm2 restart app-server --update-env" >>/home/ubuntu/lab-aws/app/server\/deploy.log
-pm2 restart app-server --update-env >>/home/ubuntu/lab-aws/app/server\/deploy.log
+echo "$(date) pm2 restart app-server --update-env" >>/home/ubuntu/lab-aws/server/deploy.log
+pm2 restart app-server --update-env >>/home/ubuntu/lab-aws/server/deploy.log
--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo pwd
-echo "$(date) run before_install.sh: " >>/home/ubuntu/lab-aws/app/server/deploy.log
+echo "$(date) run before_install.sh: " >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo "$(date) cd /home/ubuntu/lab-aws/app/server" >>/home/ubuntu/lab-aws/app/server/deploy.log
-cd /home/ubuntu/lab-aws/app/server >>/home/ubuntu/lab-aws/app/server/deploy.log
+echo "$(date) cd /home/ubuntu/lab-aws/server" >>/home/ubuntu/lab-aws/server/deploy.log
+pwd
+ls
+cd /home/ubuntu/lab-aws/server >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo "$(date) npm install" >>/home/ubuntu/lab-aws/app/server/deploy.log
-npm install >>/home/ubuntu/lab-aws/app/server/deploy.log
-npm run build >>/home/ubuntu/lab-aws/app/server/deploy.log
+echo "$(date) npm install" >>/home/ubuntu/lab-aws/server/deploy.log
+npm install >>/home/ubuntu/lab-aws/server/deploy.log
+npm run build >>/home/ubuntu/lab-aws/server/deploy.log
