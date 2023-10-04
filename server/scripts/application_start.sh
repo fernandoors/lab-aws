33375cad4623c35849e2c2322319d9b2aa331002
fernandoors92 <fernando@movemate.io>
2023-10-04 14:15:53 -0400

    fix set pm2 status on logs before restart

--- server/scripts/application_start.sh
+++ server/scripts/application_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "$(date) run application_start.sh: " >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date) pm2 status app-server" >>/home/ubuntu/lab-aws/server/deploy.log
+pm2 status app-server >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo "$(date) sudo pm2 restart app-server" >>/home/ubuntu/lab-aws/server/deploy.log
-sudo pm2 restart app-server >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date) pm2 restart app-server --update-env" >>/home/ubuntu/lab-aws/server/deploy.log
+pm2 restart app-server --update-env >>/home/ubuntu/lab-aws/server/deploy.log
