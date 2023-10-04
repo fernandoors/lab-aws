fd5f01fbda5a7a8cae827aaa3b6b5faafee2df41
fernandoors92 <fernando@movemate.io>
2023-10-04 14:31:08 -0400

    fix server folder ref

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo pwd
-echo "$(date) run before_install.sh: " >>/home/ubuntu/lab-aws/app/deploy.log
+echo "$(date) run before_install.sh: " >>/home/ubuntu/lab-aws/app/server/deploy.log
 
-echo "$(date) cd /home/ubuntu/lab-aws/app" >>/home/ubuntu/lab-aws/app/deploy.log
-cd /home/ubuntu/lab-aws/app >>/home/ubuntu/lab-aws/app/deploy.log
+echo "$(date) cd /home/ubuntu/lab-aws/app/server" >>/home/ubuntu/lab-aws/app/server/deploy.log
+cd /home/ubuntu/lab-aws/app/server >>/home/ubuntu/lab-aws/app/server/deploy.log
 
-echo "$(date) npm install" >>/home/ubuntu/lab-aws/app/deploy.log
-npm install >>/home/ubuntu/lab-aws/app/deploy.log
-npm run build >>/home/ubuntu/lab-aws/app/deploy.log
+echo "$(date) npm install" >>/home/ubuntu/lab-aws/app/server/deploy.log
+npm install >>/home/ubuntu/lab-aws/app/server/deploy.log
+npm run build >>/home/ubuntu/lab-aws/app/server/deploy.log
