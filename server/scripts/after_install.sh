7ba34e6f05641b9d58ca63018b62c4b20f6261a4
fernandoors92 <fernando@movemate.io>
2023-10-04 14:09:44 -0400

    fix set datetime on logs

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date) run after_install.sh: " >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo 'cd /home/ubuntu/nodejs-aws-codedeploy-pipeline' >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date) cd /home/ubuntu/nodejs-aws-codedeploy-pipeline" >>/home/ubuntu/lab-aws/server/deploy.log
 cd /home/ubuntu/lab-aws/server >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo 'npm install' >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date) npm install" >>/home/ubuntu/lab-aws/server/deploy.log
 npm install >>/home/ubuntu/lab-aws/server/deploy.log
 npm run build >>/home/ubuntu/lab-aws/server/deploy.log
--- server/scripts/application_start.sh
+++ server/scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date) run application_start.sh: " >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo 'pm2 restart app-server' >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date) sudo pm2 restart app-server" >>/home/ubuntu/lab-aws/server/deploy.log
 sudo pm2 restart app-server >>/home/ubuntu/lab-aws/server/deploy.log
