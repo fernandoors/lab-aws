af0c048cdb149d2a0fa4dff689f55a9bb0257c86
fernandoors92 <fernando@movemate.io>
2023-10-04 15:49:20 -0400

    check if the build update the dist folder

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -2,10 +2,14 @@
 
 echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
 echo 'run after_install.sh: ' >>/home/ubuntu/lab-aws/server/deploy.log
+
 echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
 echo 'pm2 status app-server' >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
 pm2 status app-server >>/home/ubuntu/lab-aws/server/deploy.log
 pm2 status app-server
+
 echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
 echo 'pm2 restart app-server --update-env' >>/home/ubuntu/lab-aws/server/deploy.log
 pm2 restart app-server --update-env >>/home/ubuntu/lab-aws/server/deploy.log
--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -9,6 +9,23 @@ cd /home/ubuntu/lab-aws/server >>/home/ubuntu/lab-aws/server/deploy.log
 echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
 echo 'npm install' >>/home/ubuntu/lab-aws/server/deploy.log
 npm install >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date) $(pwd)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'ls -l dist/index.js' >>/home/ubuntu/lab-aws/server/deploy.log
+ls -l dist/index.js >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date) $(pwd)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'ls -l src/index.ts ' >>/home/ubuntu/lab-aws/server/deploy.log
+ls -l src/index.ts >>/home/ubuntu/lab-aws/server/deploy.log
+
 echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
 echo 'npm build' >>/home/ubuntu/lab-aws/server/deploy.log
 npm run build >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date) $(pwd)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'ls -l dist/index.js' >>/home/ubuntu/lab-aws/server/deploy.log
+ls -l dist/index.js >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date) $(pwd)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'ls -l src/index.ts ' >>/home/ubuntu/lab-aws/server/deploy.log
+ls -l src/index.ts >>/home/ubuntu/lab-aws/server/deploy.log
