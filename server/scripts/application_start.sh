c9e86e0ec1fb9d9613d8afbb50de75b7259f6610
fernandoors92 <fernando@movemate.io>
2023-10-04 13:57:29 -0400

    fix after script to restart

--- server/scripts/application_start.sh
+++ server/scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >>/home/ubuntu/lab-aws/server/deploy.log
 
 echo 'pm2 restart app-server' >>/home/ubuntu/lab-aws/server/deploy.log
-pm2 restart app-server >>/home/ubuntu/lab-aws/server/deploy.log
+sudo pm2 restart app-server >>/home/ubuntu/lab-aws/server/deploy.log
