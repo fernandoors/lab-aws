bc19089dfc1994defa13bd770ba2fdb92980b51a
fernandoors92 <fernando@movemate.io>
2023-10-05 00:07:01 -0400

    fix force build on correct folder

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
 echo 'run before_install.sh: ' >>/home/ubuntu/lab-aws/server/deploy.log
 
-echo "$(date) 'copy dist files to project folder'" >>/home/ubuntu/lab-aws/server/deploy.log
-echo 'cp -Rf dist/ /home/ubuntu/lab-aws/server' >>/home/ubuntu/lab-aws/server/deploy.log
-cp -Rf dist/ /home/ubuntu/lab-aws/server >>/home/ubuntu/lab-aws/server/deploy.log
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'cd /home/ubuntu/lab-aws/server' >>/home/ubuntu/lab-aws/server/deploy.log
+cd /home/ubuntu/lab-aws/server >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'npm install' >>/home/ubuntu/lab-aws/server/deploy.log
+npm install >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'npx tsc -p tsconfig.json --outDir /home/ubuntu/lab-aws/server/dist' >>/home/ubuntu/lab-aws/server/deploy.log
+npx tsc -p tsconfig.json --outDir /home/ubuntu/lab-aws/server/dist >>/home/ubuntu/lab-aws/server/deploy.log
