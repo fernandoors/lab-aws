240be60357f10af77e2dad110b508c75bf60368d
fernandoors92 <fernando@movemate.io>
2023-10-05 00:19:57 -0400

    fix foce replace

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -13,3 +13,11 @@ npm install >>/home/ubuntu/lab-aws/server/deploy.log
 echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
 echo 'npx tsc -p tsconfig.json --outDir /home/ubuntu/lab-aws/server/dist' >>/home/ubuntu/lab-aws/server/deploy.log
 npx tsc -p tsconfig.json --outDir ./build >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'Replace dist content with the build: cp -Rf ./build/* /home/ubuntu/lab-aws/server/dist/' >>/home/ubuntu/lab-aws/server/deploy.log
+cp -Rf ./build/* /home/ubuntu/lab-aws/server/dist/ >>/home/ubuntu/lab-aws/server/deploy.log
+
+echo "$(date)" >>/home/ubuntu/lab-aws/server/deploy.log
+echo 'Clean build folder: rm -rf ./build/' >>/home/ubuntu/lab-aws/server/deploy.log
+rm -rf ./build/ >>/home/ubuntu/lab-aws/server/deploy.log
